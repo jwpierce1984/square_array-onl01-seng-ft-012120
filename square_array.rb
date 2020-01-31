d37680e58c0b7b162fbe065e49a8eea1c89afb0e
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 15:23:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,8 @@
 def square_array(array)
   square_array = []
-  array.each {|number| square_array << number ** 2}
+  array.each do |number|
+    square_array << number ** 2
+  square_array
+  end
+
 end
